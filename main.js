99ac75406fb6bf7566fdecb3203ddd18e07f4367
gajerabiren <gajerabiren23@gmail.com>
2021-03-04 09:39:41 +0530

    slider-added

---
+++ main.js
@@ -0,0 +1,14 @@
+let left = 1;
+let right = 4;
+
+function show() {
+
+    for(i=left; i<=right ; i++)
+    {
+    document.getElementById('image'+i).style.display = 'block';
+    }
+}
+
+function moveleft(){
+    
+}
